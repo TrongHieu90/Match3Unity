3e20198053eb22c141f339c84159f380c1c46cfa
Trong Hieu <tronghieu.designer@gmail.com>
2020-01-29 23:44:20 +0700

    + Set up game manager and core logic

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+	public ArrayLayout BoardLayout;
+	public Sprite[] Pieces;
+	private Node[,] _board;
+	private int width = 9;
+	private int height = 14;
+	private System.Random _random;
+
+
+	private void Init()
+	{
+		
+		string seed = GetRandomSeed();
+		_random = new System.Random(seed.GetHashCode());
+
+		GenerateBoard();
+
+	}
+
+	private void GenerateBoard()
+	{
+		_board = new Node[width, height];
+
+		for (int y = 0; y < height; y++)
+		{
+			for (int x = 0; x < width; x++)
+			{
+				_board[x, y] = new Node(_value: BoardLayout.rows[y].row[x] ? -1 : FillPiece(), 
+										_point: new Point(x, y)
+										);
+			}
+		}
+	}
+
+	private void VerifyBoard()
+	{
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				Point p = new Point(x, y);
+				int val = GetValueAtPoint(p);
+				if(val <= 0 ) continue;
+			}
+		}
+	}
+
+	List<Point> IsConnected(Point p, bool main)
+	{
+		List<Point> connected = new List<Point>();
+		int val = GetValueAtPoint(p);
+		var directions = Directions;
+
+		foreach (var dir in directions) //check if there is two or more same shape
+		{
+			List<Point> line = new List<Point>();
+			int same = 0;
+
+			for (int i = 1; i < 3; i++)
+			{
+				Point check = Point.Add(p, Point.Multiply(dir, i));
+				if (GetValueAtPoint(check) == val)
+				{
+					line.Add(check);
+					same++;
+				}
+			}
+
+			if (same > 1) //if there are more of the same shape in directions -> match
+			{
+				AddPoints(ref connected, line);
+			}
+		}
+
+		//TODO: continue to finish this method
+		return null;
+
+	}
+
+	private static Point[] Directions
+	{
+		get
+		{
+			Point[] directions =
+			{
+				Point.Up,
+				Point.Down,
+				Point.Left,
+				Point.Right
+			};
+			return directions;
+		}
+	}
+
+	private void AddPoints(ref List<Point> connected, List<Point> line)
+	{
+		throw new NotImplementedException();
+	}
+
+	private int GetValueAtPoint(Point point)
+	{
+		return _board[point.x, point.y].value;
+	}
+
+	private int FillPiece()
+	{
+		int val = 1;
+		val = (_random.Next(0, 100)) / (100 / Pieces.Length) + 1;
+		return val;
+	}
+
+	private string GetRandomSeed()
+	{
+		string seed = String.Empty;
+		string acceptableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()";
+
+		for (int i = 0; i < acceptableChars.Length; i++)
+		{
+			seed += acceptableChars[Random.Range(0, acceptableChars.Length)];
+		}
+
+		return seed;
+	}
+}
+
+[System.Serializable]
+public class Node
+{
+	//0 = blank,
+	//1 = cube,
+	//2 = sphere,
+	//3 = cylinder,
+	//4 = pyramid,
+	//5 = diamond,
+	//-1 = hole
+	public int value;
+
+	public Point index;
+
+	public Node(int _value, Point _point)
+	{
+		value = _value;
+		index = _point;
+	}
+}
+
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Point
+{
+	public int x { get; private set; }
+	public int y { get; private set; }
+
+	public Point(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public void  Multiply(int m)
+	{
+		x *= m; 
+		y *= m;
+	}
+
+	public void  Add( Point o)
+	{
+		x += o.x; 
+		y += o.y;
+	}
+
+	public bool Equal(Point p)
+	{
+		return p.x == x && p.y == y;
+	}
+
+	public static Point FromVector(Vector2 v)
+	{
+		return new Point((int)(v.x), (int)(v.y));
+	}
+
+	public static Point FromVector(Vector3 v)
+	{
+		return new Point((int)(v.x), (int)(v.y));
+	}
+
+	public static Point Multiply(Point p, int m)
+	{
+		return new Point(p.x *m , p.y * m);
+	}
+
+	public static Point Add(Point p, Point o)
+	{
+		return new Point(p.x + o.x, p.y + o.y);
+	}
+
+	public static Point Clone(Point p)
+	{
+		return new Point(p.x, p.y);
+	}
+
+	public static Point Zero => new Point(0, 0);
+
+	public static Point Up => new Point(0, 1);
+
+	public static Point Down => new Point(0, -1);
+
+	public static Point Left => new Point(-1, 0);
+
+	public static Point Right => new Point(1, 0);
+}
